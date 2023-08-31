98659c45d18aea1a3de5af144568e266be6ab719
Godllolzzz <sudarshanrajpit2002@gamil.com>
2023-08-31 15:28:24 +0530

    new tem

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,10 @@
 //Your JavaScript goes in here
+function togglePower(button) {
+    button.classList.toggle('on');
+    document.getElementById('off').style.color = "white";
+    document.getElementById('on').style.color = "green";
+}
+const sample_placing_handler = (button) => {
+    button.classList.remove('btn-danger');
+    button.classList.add('btn-success');
+}
